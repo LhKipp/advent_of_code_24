a14e7911d854a6864548dbdf0b9de568ab8b13bb
Leonhard Kipp <Leonhard.Kipp@alumni.fh-aachen.de>
2025-01-03 17:13:04 +0100

    day17 part 2

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct ProgramState {
     a: i64,
     b: i64,
@@ -12,6 +12,12 @@ struct ProgramState {
 }
 
 impl ProgramState {
+    pub fn reset(&mut self) {
+        self.b = 0;
+        self.c = 0;
+        self.i = 0;
+    }
+
     pub fn literal_op_at(&self, i: usize) -> i64 {
         self.instructions[i]
     }
@@ -65,13 +71,11 @@ fn parse_input(file_path: &str) -> io::Result<ProgramState> {
 fn bxl(program: &mut ProgramState) {
     let operand = program.literal_op_at(program.i + 1);
     program.b ^= operand;
-    program.i += 2;
 }
 
 fn bst(program: &mut ProgramState) {
     let operand = program.combo_op_at(program.i + 1);
     program.b = operand % 8;
-    program.i += 2;
 }
 
 fn jnz(program: &mut ProgramState) {
@@ -86,56 +90,54 @@ fn jnz(program: &mut ProgramState) {
 
 fn bxc(program: &mut ProgramState) {
     program.b ^= program.c;
-    program.i += 2;
 }
 
 fn out(program: &mut ProgramState) {
     let operand = program.combo_op_at(program.i + 1);
     program.out.push(operand % 8);
-    program.i += 2;
 }
 
-fn division(program: &mut ProgramState) -> i64 {
-    let operand = program.combo_op_at(program.i + 1);
-    let bottom = 1 << operand;
-    let vvv = program.a / bottom;
-    program.i += 2;
-    // println!("{} = {} / {}<<{} ({})", vvv, program.a, 2, operand, 2 << operand);
-    vvv
-}
-
-fn execute_program(program: &mut ProgramState) {
+fn execute_program(program: &mut ProgramState) -> Vec<i64> {
     while (program.i + 1) < program.instructions.len() {
-        println!("{:?}", program);
-        match program.instructions[program.i] {
-            0 => {
-                program.a = division(program);
-            }
+        let op = program.instructions[program.i];
+        match op {
+            0 => program.a >>= program.combo_op_at(program.i + 1),
             1 => bxl(program),
             2 => bst(program),
             3 => jnz(program),
             4 => bxc(program),
             5 => out(program),
-            6 => program.b = division(program),
-            7 => program.c = division(program),
+            6 => program.b = program.a >> program.combo_op_at(program.i + 1),
+            7 => program.c = program.a >> program.combo_op_at(program.i + 1),
             _ => unreachable!("found non opcode"),
         }
+        if op != 3 {
+            program.i += 2;
+        }
     }
+    std::mem::take(&mut program.out)
 }
 
 fn main() -> io::Result<()> {
     let mut program_state = parse_input(&std::env::args().nth(1).unwrap())?;
-    execute_program(&mut program_state);
 
-    println!("program finished");
-    println!("{:?}", program_state);
-    let result = program_state
-        .out
-        .iter()
-        .map(|n| n.to_string())
-        .collect::<Vec<String>>()
-        .join(",");
-    println!("{}", result);
+    let mut a = 0;
+    for i in (0..program_state.instructions.len()).rev() {
+        a <<= 3;
+        let instructions: Vec<i64> = program_state.instructions[i..].to_vec();
+        loop {
+            program_state.reset();
+            program_state.a = a;
+            let out_test = execute_program(&mut program_state);
+            println!("A {}: testing {:?} == {:?}", a, out_test, instructions);
+            if out_test == instructions {
+                break;
+            }
+            a += 1;
+        }
+    }
+
+    println!("{}", a);
 
     Ok(())
 }
