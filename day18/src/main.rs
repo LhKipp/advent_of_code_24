14c088c46b0e0206fe0cf00142e7f56623edbae7
Leonhard Kipp <Leonhard.Kipp@alumni.fh-aachen.de>
2025-01-03 18:46:44 +0100

    day18 part2

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,14 +1,15 @@
 use std::env;
+use std::fmt::Debug;
 use std::fs::File;
 use std::io::{self, BufRead};
 
-use petgraph::algo::dijkstra;
+use petgraph::algo::astar;
 use petgraph::graph::{NodeIndex, UnGraph};
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
 struct Pos {
-    pub x: i32,
-    pub y: i32,
+    pub x: usize,
+    pub y: usize,
 }
 
 fn parse_positions_from_file(filename: &str) -> Vec<Pos> {
@@ -25,8 +26,8 @@ fn parse_positions_from_file(filename: &str) -> Vec<Pos> {
             .collect();
         if parts.len() == 2 {
             pos_list.push(Pos {
-                x: parts[0],
-                y: parts[1],
+                x: parts[0] as usize,
+                y: parts[1] as usize,
             });
         }
     }
@@ -34,56 +35,77 @@ fn parse_positions_from_file(filename: &str) -> Vec<Pos> {
     pos_list
 }
 
-fn grid_after(falling_bytes: &[Pos], steps: usize) -> Vec<Vec<char>> {
-    let mut result = vec![vec!['.'; 71]; 71];
-    for falling_byte in &falling_bytes[0..steps] {
-        result[falling_byte.x as usize][falling_byte.y as usize] = '#';
-    }
-    result
-}
-
-type G = UnGraph<(usize, usize), ()>;
+type G = UnGraph<Pos, ()>;
 type NIdx = NodeIndex<u32>;
 
-fn is_visitable(grid: &[Vec<char>], x: usize, y: usize) -> bool {
-    return grid
+fn is_visitable(nodeidx: &[Vec<Option<NIdx>>], x: usize, y: usize) -> bool {
+    return nodeidx
         .get(x)
-        .and_then(|v| v.get(y))
-        .is_some_and(|v| *v == '.');
+        .and_then(|v| v.get(y).cloned())
+        .is_some_and(|v| v.is_some());
 }
 
-fn shortest_path(grid: &[Vec<char>]) -> i32 {
+fn graph_for(falling_bytes: &[Pos]) -> (G, Vec<Vec<Option<NIdx>>>) {
     let mut graph = G::new_undirected();
     let mut nodeidx: Vec<Vec<Option<NIdx>>> = vec![vec![None; 71]; 71];
-
-    for x in 0..=70 {
+    (0..=70).for_each(|x| {
         for y in 0..=70 {
-            if grid[x][y] != '#' {
-                nodeidx[x][y] = Some(graph.add_node((x, y)));
+            if !falling_bytes.contains(&Pos { x, y }) {
+                // println!("assigning {},{}", x,y);
+                nodeidx[x][y] = Some(graph.add_node(Pos { x, y }));
             }
         }
-    }
+    });
     for x in 0..=70 {
         for y in 0..=70 {
-            if grid[x][y] != '#' {
-                if is_visitable(grid, x + 1, y) {
+            if !falling_bytes.contains(&Pos { x, y }) {
+                if x < 70 && is_visitable(&nodeidx, x + 1, y) {
                     graph.add_edge(nodeidx[x][y].unwrap(), nodeidx[x + 1][y].unwrap(), ());
                 }
-                if is_visitable(grid, x.wrapping_sub(1), y) {
-                    graph.add_edge(nodeidx[x][y].unwrap(), nodeidx[x - 1][y].unwrap(), ());
-                }
-                if is_visitable(grid, x, y + 1) {
+                // if x > 0 {
+                //     graph.add_edge(nodeidx[x][y], nodeidx[x - 1][y], ());
+                // }
+                if y < 70 && is_visitable(&nodeidx, x, y + 1) {
                     graph.add_edge(nodeidx[x][y].unwrap(), nodeidx[x][y + 1].unwrap(), ());
                 }
-                if is_visitable(grid, x, y.wrapping_sub(1)) {
-                    graph.add_edge(nodeidx[x][y].unwrap(), nodeidx[x][y - 1].unwrap(), ());
-                }
+                // if y > 0 {
+                //     graph.add_edge(nodeidx[x][y], nodeidx[x][y - 1], ());
+                // }
+            }
+        }
+    }
+    (graph, nodeidx)
+}
+
+fn shortest_path(falling_bytes: &[Pos]) -> Pos {
+    let mut falling_bytes_i = 0;
+    let (mut graph, mut nodeidx) = graph_for(&falling_bytes[0..falling_bytes_i]);
+
+    while let Some(path) = astar(
+        &graph,
+        nodeidx[0][0].unwrap(),
+        |finish| finish == nodeidx[70][70].unwrap(),
+        |_| 1,
+        |_| 0,
+    ) {
+        let path_positions: Vec<Pos> = path
+            .1
+            .iter()
+            .map(|n| *graph.node_weight(*n).unwrap())
+            .collect();
+        println!("Found path: {:?}", path_positions);
+        for _ in 0..99999 {
+            let falling_byte = falling_bytes[falling_bytes_i];
+            falling_bytes_i += 1;
+            if path_positions.contains(&falling_byte) {
+                println!("Falling byte {:?} blocks path", falling_byte);
+                (graph, nodeidx) = graph_for(&falling_bytes[0..falling_bytes_i]);
+                break;
             }
         }
     }
 
-    let costs = dijkstra(&graph, nodeidx[0][0].unwrap(), nodeidx[70][70], |_| 1);
-    costs[&nodeidx[70][70].unwrap()]
+    falling_bytes[falling_bytes_i - 1]
 }
 
 fn main() {
@@ -93,8 +115,13 @@ fn main() {
 
     // Parse positions from the file and print them
     let pos_list = parse_positions_from_file(filename);
-    let grid = grid_after(&pos_list, 1024);
-    let cost = shortest_path(&grid);
+    let cost = shortest_path(&pos_list);
 
-    println!("{}", cost);
+    println!("{:?}", cost);
+}
+
+impl Debug for Pos {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({},{})", self.x, self.y)
+    }
 }
