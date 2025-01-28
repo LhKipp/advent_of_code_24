98c9c8fe65174e0429b090e018f347896332c1d6
Leonhard Kipp <Leonhard.Kipp@alumni.fh-aachen.de>
2025-01-28 20:15:51 +0100

    day23 part1

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -1,14 +1,22 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead};
 
 use itertools::Itertools;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
 struct Edge {
     pub from: String,
     pub to: String,
+}
+impl Edge {
+    fn reverse(&self) -> Edge {
+        Self {
+            to: self.from.clone(),
+            from: self.to.clone(),
+        }
+    }
 }
 
 fn parse_edges(filepath: &str) -> Vec<Edge> {
@@ -26,6 +34,7 @@ fn parse_edges(filepath: &str) -> Vec<Edge> {
                 from: parts[0].to_string(),
                 to: parts[1].to_string(),
             };
+            edges.push(edge.reverse());
             edges.push(edge);
         }
     }
@@ -61,21 +70,27 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let filepath = &args[1];
 
-    let edges = parse_edges(filepath);
+    let edges = parse_edges(filepath)
+        .into_iter()
+        .into_group_map_by(|e| e.from.clone());
+    let edges_set: HashSet<Edge> = edges.iter().flat_map(|e| e.1.clone()).collect();
 
-    let parents = union_find(&edges);
-
-    println!("{:?}", parents);
-    let mut groups = vec![];
-    for (_, chunk) in &parents.into_iter().chunk_by(|p| p.1.clone()) {
-        groups.push(chunk.collect_vec());
+    let mut result = vec![];
+    for (from, edges) in edges {
+        if from.starts_with('t') {
+            for comb in edges.iter().combinations(2) {
+                if edges_set.contains(&Edge {
+                    from: comb[0].to.clone(),
+                    to: comb[1].to.clone(),
+                }) {
+                    let mut cc = vec![from.clone(), comb[0].to.clone(), comb[1].to.clone()];
+                    cc.sort();
+                    result.push(cc);
+                }
+            }
+        }
     }
-
-    let count = groups
-        .iter()
-        .filter(|group| group.len() == 3 && group.iter().any(|(n, _)| n.starts_with('t')))
-        .inspect(|g| println!("{:?}", g))
-        .count();
-
-    println!("{}", count);
+    result = result.iter().unique().cloned().collect_vec();
+    println!("{:?}", result);
+    println!("{}", result.len());
 }
